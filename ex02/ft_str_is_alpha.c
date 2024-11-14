211440c7c93f6a364e04597a297f20fd50d552e7
Giampiero Andrea Ruggiero <giamrugg@e3r3p5.42roma.it>
2024-11-14 19:47:18 +0100

    inizio correzione norma di ex02

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: giamrugg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:17:45 by giamrugg          #+#    #+#             */
-/*   Updated: 2024/11/14 17:04:05 by giamrugg         ###   ########.fr       */
+/*   Updated: 2024/11/14 19:45:05 by giamrugg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int	ft_str_is_alpha(char *str)
 {
-	int i = 0;
-	int j = 1;
-	
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
 	while (*str)
 	{
-		if (!((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z') || (*str == '\0')))
+		if (!(*str >= 'a' && *str <= 'z'))
+		if (!(*str >= 'A' && *str <= 'Z'))
+		if (!(*str == '\0'))
 		{
 			return(i);
 		}
